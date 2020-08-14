d1248893d66005adb572bf21eeaf1451f590dce3
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-08-14 12:46:30 +0200

    Ejercicio2

--- app/src/main/java/com/cbellmont/ejemploadaptersinlista/StringAdapter.kt
+++ app/src/main/java/com/cbellmont/ejemploadaptersinlista/StringAdapter.kt
@@ -4,12 +4,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class StringAdapter : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  {
 
     class StringViewHolder(var root: View, var textView: TextView) : RecyclerView.ViewHolder(root)
+
+    var numeroDeFilas = 10
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
@@ -18,7 +21,7 @@ class StringAdapter : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  {
     }
 
     override fun getItemCount(): Int {
-        return 10
+        return numeroDeFilas
     }
 
     override fun onBindViewHolder(holder: StringViewHolder, position: Int) {
@@ -26,11 +29,17 @@ class StringAdapter : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  {
             holder.textView.setBackgroundColor(ContextCompat.getColor(holder.root.context, R.color.colorPrimaryDark))
             "Position = $position"
         } else {
+            holder.textView.setBackgroundColor(ContextCompat.getColor(holder.root.context, R.color.colorAccent))
             "Soy impar"
         }
 
         if (position == itemCount -1) {
-            holder.textView.text = "Soy el ultimo"
+            holder.textView.text = "Add"
+            holder.textView.setOnClickListener {
+                Toast.makeText(holder.root.context, "Adding", Toast.LENGTH_LONG).show()
+                numeroDeFilas++
+                notifyDataSetChanged()
+            }
         }
     }
 
